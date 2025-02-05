88e11eadf7f6d3341d9252cf790b7ea242d5b351
victsilva37 <victormau.sm@gmail.com>
2025-02-05 15:53:18 -0300

    Se enlaza el banner con el frontend

---
+++ main.php
@@ -0,0 +1,63 @@
+<!--
+- Envases y Troquelados: BLANQUITA MELIPILLA
+- Sitio web: https://www.blanquita_melipilla.com
+- Desarrollador: DeV37
+--->
+
+<!--------------------------------------------------------------------------------------
+  ---------------------------Inicio Main .PHP-------------------------------------------
+  -------------------------------------------------------------------------------------->
+
+<!-------------------
+  ---Inicio LÓGICA---
+  ------------------->
+
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Link archivo CSS-->
+    <link rel="stylesheet" href="style.css">
+
+    <!--Link estilos de bootstraps-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+</head>
+
+  <body>
+
+    <!--**Estructura del componente PHP**-->
+
+        <!--SECCIÓN: BANNER-->
+
+            <?php include 'php/banner.php'?>
+
+
+        <!--*NOMBRE DE LA SECCIÓN 2-->
+
+            <!--*****Código 2*****-->
+
+    <!--Link script de bootstrap-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!--**Link archivo JS**-->
+        <script src=""></script>
+
+  </body>
+  </html>
+
+<!-------------------
+  --- Fin LÓGICA  ---
+  ------------------->
+
+<!--------------------------------------------------------------------------------------
+  ------------------------------Fin Main .PHP-------------------------------------------
+  -------------------------------------------------------------------------------------->
+
+<!--
+- Envases y Troquelados: BLANQUITA MELIPILLA
+- Sitio web: https://www.blanquita_melipilla.com
+- Desarrollador: DeV37
+--->
