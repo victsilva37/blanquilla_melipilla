02b330acc77146f7c0e2ccfdcc195f8bcdebf8d9
victsilva37 <victormau.sm@gmail.com>
2025-02-06 15:03:30 -0300

    Se coloca include para llamar a los archivos

--- main.php
+++ main.php
@@ -17,8 +17,6 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--Link archivo CSS-->
-    <link rel="stylesheet" href="style.css">
 
     <!--Link estilos de bootstraps-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -29,21 +27,18 @@
   <body>
 
     <!--**Estructura del componente PHP**-->
-
+    
         <!--SECCIÓN: BANNER-->
 
-            <?php include 'php/banner.php'?>
+          <?php include 'php/banner.php'?>
 
 
-        <!--*NOMBRE DE LA SECCIÓN 2-->
+        <!--SECCIÓN: CATÁLOGO-->
 
-            <!--*****Código 2*****-->
+          <?php include 'php/catalogo.php'?>
 
     <!--Link script de bootstrap-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-    <!--**Link archivo JS**-->
-        <script src=""></script>
 
   </body>
   </html>
